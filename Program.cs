c1776a31fb36ed60e9dc552044816648c43dcfcb
Ekischleki <e.wolfschlaeger@gmail.com>
2023-09-27 21:18:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using PupilmoniumFights;
+
+public static class Base
+{
+    private static void Main(string[] args)
+    {
+        var result = AttackTranslate.SaveCurrentTranslationRegion("en");
+        AttackTranslate.InitTranslate(result);
+    }
+}
