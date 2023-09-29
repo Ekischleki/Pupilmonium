a472eaa79bd24ae536038b7793be962e12327067
Ekischleki <e.wolfschlaeger@gmail.com>
2023-09-29 22:17:14 +0200

    Decreased space req by shortening names

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿using PupilmoniumFights;
+using System.Diagnostics;
 
 public static class Base
 {
     private static void Main(string[] args)
     {
-        var result = AttackTranslate.SaveCurrentTranslationRegion("en");
-        AttackTranslate.InitTranslate(result);
+#if DEBUG
+        string currentPath = "C:\\Users\\ewolf\\source\\repos\\PupilmoniumBase";
+        string translationPath = Path.Combine(currentPath, "recourses\\translation\\Translation_en");
+        File.WriteAllText(translationPath, Translation.AttackTranslate.SaveCurrentTranslationRegion("en").RegionSaveString);
+        Translation.AttackTranslate.InitTranslate(translationPath);
+
+#endif
+
+        Console.WriteLine("Pupilmonium - Pupil (student) (pande)monium (wild and noisy disorder or confusion) ");
+        Console.WriteLine(Translation.AttackTranslate.Credits);
+        Console.ReadKey();
+
+
     }
 }
